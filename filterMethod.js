641d20dfdda753c82b563dea6f4857399eba7f3a
Mane <karma@softwareag.com>
2022-04-12 12:55:15 +0530

    formating,deleted filterdataFile, Deleted history file data

--- filterMethod.js
+++ filterMethod.js
@@ -1,55 +1,61 @@
 const https = require('https');
 const fs = require("fs");
 
-function getFilter(type,value,url){
-    return new Promise(resolve =>{
+function getFilter(type, value, arrFileName) {
+    return new Promise(resolve => {
         fs.readFile('./uploadedFilteredData/dataFile.txt', "utf8", (err, data) => {
             let newData = JSON.parse(data);
-                //creating new array to store filter data 
-                let arr = [];
-                //code for filtering of data 
-                for (x of newData){
-                    for (y in x){
-                        if(y === type && x[y] === value){
-                            arr.push(x);
-                            break;
-                        }
+            //creating new array to store filter data 
+            let arr = [];
+            //code for filtering of data 
+            for (x of newData) {
+                for (y in x) {
+                    if (y === type && x[y] === value) {
+                        arr.push(x);
+                        break;
                     }
                 }
+            }
+            if (arr.length <= 0) {
+                console.log("Enter Valid Input")
+            }
+            else {
                 //print the filter data in table format.
                 console.table(arr);
-                
-                //function to genrate random number for creation of file .
-    
-                function randomNumber(min, max) { 
-                    return Math.floor(Math.random() * (max - min) + min);
-                } 
-                //converting the filtered data to string to stored in a .txt file
-                let output = JSON.stringify(arr);
-                let fileName1 = 'filterDataFile' + randomNumber(1,100);
+            }
+            //function to genrate random number for creation of file .
+
+            function randomNumber(min, max) {
+                return Math.floor(Math.random() * (max - min) + min);
+            }
+            //converting the filtered data to string to stored in a .txt file
+            let output = JSON.stringify(arr);
+            let fileName1 = 'filterDataFile' + randomNumber(1, 1000000000);
+
+            arrFileName.push(fileName1);
 
 
-                //apending to of data to new file 
-                
-                fs.appendFile(`./uploadedFilteredData/${fileName1}.txt`,output, function (err) {
+            //apending to of data to new file 
+
+            fs.appendFile(`./uploadedFilteredData/${fileName1}.txt`, output, function (err) {
                 if (err) return console.log(err);
                 console.log(`./uploadedFilteredData/${fileName1}.txt`);
-                
-                });
-                //apending the user input in history.txt file 
-                fs.appendFile('./uploadedFilteredData/History.txt',"Filter : " + type + " " + value + "\n", async function (err) {
-                    if (err) return console.log(err);
-                    console.log('./uploadedFilteredData/History.txt');
-                    resolve();
-                   
-                });
-                
-    
-                
-        
+
+            });
+            //apending the user input in history.txt file 
+            fs.appendFile('./uploadedFilteredData/History.txt', "Filter : " + type + " " + value + "\n", async function (err) {
+                if (err) return console.log(err);
+                console.log('./uploadedFilteredData/History.txt');
+                resolve();
+
+            });
+
+
+
+
         })
 
     })
-   };
+};
 
 module.exports.getFilter = getFilter;
